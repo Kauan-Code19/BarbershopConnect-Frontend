3821a2dcba80d634331430c89c088a58ecbc46f7
Kauan-Code19 <pkauprofissional@gmail.com>
2024-01-29 22:40:50 -0300

    estilização e responsividade

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
